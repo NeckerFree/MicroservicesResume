6f9b7d1bcaa0a0662fbeec45d28acb8028083f16
Elio Cortés <elio.cortes3000@gmail.com>
2023-06-12 23:49:24 -0500

    added Auth service

---
+++ src/Consumer.API/Entities/Client.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Parser.Common.Entities;
+
+namespace Consumer.API.Entities
+{
+    public class Client : IEntity
+    {
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTimeOffset? Created { get; set; }
+    }
+}
